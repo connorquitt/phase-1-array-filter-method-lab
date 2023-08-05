8b4feda691502434ddce88cdf4188fa6a256a931
connorq <connorquitt@gmail.com>
2023-08-05 17:45:20 -0500

    all but the last one finished

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching (arr, name) {
+    return(arr.filter(function(string) {
+        if(name.toLowerCase() === string.toLowerCase()) {
+            return string
+        }
+    }))
+}
+
+function fuzzyMatch (arr, char) {
+    return arr.filter(function(string) {
+        if(string[0, 1] === char[0, 1]){
+            return string
+        }
+    })
+}
+   
+function matchName (arr, name) {
+    
+}
+//matchName(drivers, 'Bobby')
+
+(drivers.filter.name(function(info){
+    if('Sally' === drivers.name){
+        console.writeline('hi')
+    }
+}))
+
+
